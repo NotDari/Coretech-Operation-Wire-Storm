ffd7c1eb3a1c5d60ba5c4611d393dbc3920e279e
dari <dariushtomlinson@gmail.com>
2025-08-09 13:00:42 +0100

    Retrieves length of message from source client

--- main.cpp
+++ main.cpp
@@ -21,14 +21,33 @@ int main() {
     listen(serverSocket, 5);
     cout << "Listening for connection: " << endl;
 
-    sockaddr_in clientAddress;
-    socklen_t clenIn = sizeof(clientAddress);
+    sockaddr_in sourceClientAddress;
+    socklen_t sourceAddressLength = sizeof(sourceClientAddress);
 
-    int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clenIn);
+    int clientSocket = accept(serverSocket, (struct sockaddr *) &sourceClientAddress, &sourceAddressLength);
 
-    cout << "Client connected + ip:" << clientAddress.sin_addr.s_addr << endl;
+    int bufferSize = 4096;
+    uint8_t buffer[bufferSize];
+    const uint8_t magicByte = 0xCC;
 
+    while (true) {
+        ssize_t headerBytes = recv(clientSocket, buffer, 8, 0 );
+        if (headerBytes < 8) {
+            //ERROR HANDLING;
+        }
+        if (buffer[0] != magicByte) {
+            //No magic byte. THrow error
+        }
+        //TODO Check if specific build version is little endian
+        int length = (buffer[3] << 8) +  buffer[2];
+
+        cout << "Length received:" << length << endl;
+        break;
+
+    }
     close(serverSocket);
     return 0;
 }
+
+
 
