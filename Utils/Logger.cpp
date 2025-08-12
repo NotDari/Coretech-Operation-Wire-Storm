dca215e2fd4c21d47e5269fa7c0c9933ae24cd42
dari <dariushtomlinson@gmail.com>
2025-08-12 17:59:06 +0100

    Log wasn't correctly added in last commit.

--- Utils/Logger.cpp
+++ Utils/Logger.cpp
@@ -1,3 +1,25 @@
 //
-// Created by Dariush Tomlinson on 11/08/2025.
+// Created by Dariush Tomlinson on 12/08/2025.
 //
+#include "Logger.h"
+
+std::mutex Logger::logMutex;
+
+std::string Logger::convertLoggerLevelString(LoggerLevel level){
+    switch (level) {
+        case LoggerLevel::INFO:
+            return "INFO";
+        case LoggerLevel::DEBUG:
+            return "DEBUG";
+        case LoggerLevel::WARN:
+            return "WARN";
+        default: return "ERROR";
+    }
+}
+
+
+void Logger::log(std::string message, LoggerLevel level){
+    std::lock_guard<std::mutex> guard(logMutex);
+    auto time = std::chrono::system_clock::now();
+    std::cout << "[" << convertLoggerLevelString(level) << "] " << time << " : " << message << std::endl;
+}
